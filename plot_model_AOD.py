4166c76f1a5840a8801cebb13c311adf75a46297
Athira-lab-24 <athira.pt.atmos@gmail.com>
2026-09-09 11:04:43 +0530

    Update plot_model_AOD.py

--- plot_model_AOD.py
+++ plot_model_AOD.py
@@ -1,8 +1,18 @@
+# Author: Athira P T
+# MSc Dissertation: Dust Modelling over the Indian Region
+# Description: Plot model-simulated AOD using the CLASSIC/UKCA schemes
+
 import xarray as xr
 import matplotlib.pyplot as plt
 import geopandas as gpd
 
-ds=xr.open_dataset(r"C:\Users\athir\Pictures\New folder\OneDrive\Desktop\NCMRWF\RESULTS\AOD\dod_20171107_NOV_AOD.nc")
+
+
+import xarray as xr
+import matplotlib.pyplot as plt
+import geopandas as gpd
+
+ds=xr.open_dataset(r"..\data\dod_20171107_NOV_AOD.nc")
 #print (ds)
 
 AOD_CLASSIC = ds['atmosphere_optical_thickness_due_to_aerosol']
@@ -18,7 +28,7 @@ AOD.plot(
     cbar_kwargs ={'label':'AOD'}
 )
 india = gpd.read_file(
-    r"C:\Users\athir\Downloads\maps-master\maps-master\States\Admin2.shp"
+    r"..\shapefiles\Admin2.shp"
 )
 india.boundary.plot(
     ax=plt.gca(),
@@ -27,5 +37,5 @@ india.boundary.plot(
 )
 plt.xlabel('longitude')
 plt.ylabel('latitude')
-plt.title('AOD from model CLASSIC scheme')
+plt.title('AOD from model -CLASSIC scheme')
 plt.show()
